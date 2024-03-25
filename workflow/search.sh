e0eebec02a946a33b0c0dd31efc8f6bbd5416369
hoho4190 <89823453+hoho4190@users.noreply.github.com>
2024-03-25 10:28:32 +0900

    fix: Update bookmark path parsing

--- workflow/search.sh
+++ workflow/search.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# see https://docs.cyberduck.io/cyberduck/faq/#preferences-and-application-support-files-location
-readonly BOOKMARK_PATH="${APP_SUPPORT_PATH/#~/$HOME}Bookmarks"
 readonly QUERY=$(tr '[A-Z]' '[a-z]' <<<"$1")
-
 readonly ITEM_TEMPLATE='{"title": "!@#title", "subtitle": "!@#subtitle", "icon": {"path": "assets/images/bookmark.png"}, "arg": "!@#arg"},'
+
+get_bookmark_path() {
+    local path="${1/#~/$HOME}"
+    [[ "${path: -1}" != "/" ]] && path+="/"
+    path=$(sed 's/\\ / /g' <<<"$path")
+    echo "${path}Bookmarks"
+}
 
 get_item() {
     local path=$1
@@ -46,6 +50,9 @@ get_item() {
 
 ###########################
 ## MAIN
+
+# see https://docs.cyberduck.io/cyberduck/faq/#preferences-and-application-support-files-location
+readonly BOOKMARK_PATH=$(get_bookmark_path "$APP_SUPPORT_PATH")
 
 declare -a bookmark_paths=("$BOOKMARK_PATH"/*)
 declare -a bookmarks=()
